e1bf43cb1e7b7b0479c42b8d68a8f888b2c4f98d
Ronald Vega <ronald.vega48@gmail.com>
2020-08-17 17:32:35 -0600

    Loader changed

--- scripts.js
+++ scripts.js
@@ -9,3 +9,35 @@ selectElement('.open').addEventListener('click', () => {
 selectElement('.close').addEventListener('click', () => {
     selectElement('.nav-list').classList.remove('active');
 })
+
+//Scroll reveal
+
+window.sr = new ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1100,
+    distance: '25rem',
+    delay: 60
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1100,
+    distance: '25rem',
+    delay: 60
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '8rem',
+    delay: 40
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '10rem',
+    delay: 45
+});
